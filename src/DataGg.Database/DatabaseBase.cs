50300c24f87cdfaf77319d9c6b57f55f8991bda4
Jason Magee <jason@cortex.gg>
2021-05-07 08:50:57 +0100

    init

---
+++ src/DataGg.Database/DatabaseBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGg.Database
+{
+    public abstract class DatabaseBase
+    {
+        protected readonly string _connStr;
+
+        internal DatabaseBase(IConfiguration configuration)
+        {
+            _connStr = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected async Task<SqlConnection> OpenConnectionAsync()
+        {
+            return await OpenConnectionAsync(_connStr);
+        }
+
+        public static async Task<SqlConnection> OpenConnectionAsync(string connStr)
+        {
+            var connection = new SqlConnection(connStr);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
