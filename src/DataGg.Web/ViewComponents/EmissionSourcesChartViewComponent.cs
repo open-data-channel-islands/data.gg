80092cbe9caf301890d3e4b72c87c2345be5a861
Jason Magee <deedoubleyou@gmail.com>
2022-03-03 20:46:05 +0000

    Added emission charts

---
+++ src/DataGg.Web/ViewComponents/EmissionSourcesChartViewComponent.cs
@@ -0,0 +1,76 @@
+﻿using DataGg.Core.Types;
+using DataGg.Web.Models;
+using DataGg.Web.Services;
+using Highsoft.Web.Mvc.Charts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGg.Web.ViewComponents
+{
+    public class EmissionSourcesChartViewComponent : ViewComponent
+    {
+
+        private readonly CacheManager _cacheManager;
+
+        public EmissionSourcesChartViewComponent(CacheManager cacheManager)
+        {
+
+            _cacheManager = cacheManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var dataCache = await _cacheManager.DataCache.Get();
+
+            var model = new LineChartViewModel() { TitleText = "Sources", YAxisTitleText = "Sources", Id = "chartSources" };
+
+            var energyPowerGeneration = new List<LineSeriesData>();
+            var energyIndustrialCombustion = new List<LineSeriesData>();
+            var energyTransport = new List<LineSeriesData>();
+            var energyCommercialAndDomesticCombusiton = new List<LineSeriesData>();
+            var agriculturalLandUseLandUseChangeAndForestry = new List<LineSeriesData>();
+            var waste = new List<LineSeriesData>();
+            var fluorinatedGases = new List<LineSeriesData>();
+            var other = new List<LineSeriesData>();
+
+            model.Data = new List<Series>
+            {
+                new LineSeries { Name = "Power Generation", Data = energyPowerGeneration },
+                new LineSeries { Name = "Industrial Combustion", Data = energyIndustrialCombustion },
+                new LineSeries { Name = "Transport", Data = energyTransport },
+                new LineSeries
+                    { Name = "Commercial & Domestic Combustion", Data = energyCommercialAndDomesticCombusiton },
+                new LineSeries
+                {
+                    Name = "Agricultural Land Use and Use Change And Forestry",
+                    Data = agriculturalLandUseLandUseChangeAndForestry
+                },
+                new LineSeries { Name = "Waste", Data = waste },
+                new LineSeries { Name = "Fluorinated Gases", Data = fluorinatedGases },
+                new LineSeries { Name = "other", Data = other },
+            };
+            
+            model.Labels = new List<string>();
+
+            foreach (var qtr in dataCache.EmissionSource.OrderBy(y => y.Year))
+            {
+                energyPowerGeneration.Add(new LineSeriesData { Y = qtr.EnergyPowerGeneration });
+                energyIndustrialCombustion.Add(new LineSeriesData { Y = qtr.EnergyIndustrialCombustion });
+                energyTransport.Add(new LineSeriesData { Y = qtr.EnergyTransport });
+                energyCommercialAndDomesticCombusiton.Add(new LineSeriesData { Y = qtr.EnergyCommercialAndDomesticCombusiton });
+                agriculturalLandUseLandUseChangeAndForestry.Add(new LineSeriesData { Y = qtr.AgriculturalLandUseLandUseChangeAndForestry });
+                waste.Add(new LineSeriesData { Y = qtr.Waste });
+                fluorinatedGases.Add(new LineSeriesData { Y = qtr.FluorinatedGases });
+                other.Add(new LineSeriesData { Y = qtr.Other });
+                
+                model.Labels.Add($"{qtr.Year}");
+
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ src/DataGg.Web/ViewComponents/EmissionTypesChartViewComponent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataGg.Web.Models;
+using DataGg.Web.Services;
+using Highsoft.Web.Mvc.Charts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataGg.Web.ViewComponents;
+
+public class EmissionTypesChartViewComponent : ViewComponent
+{
+    private readonly CacheManager _cacheManager;
+
+    public EmissionTypesChartViewComponent(CacheManager cacheManager)
+    {
+        _cacheManager = cacheManager;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var dataCache = await _cacheManager.DataCache.Get();
+
+        var model = new LineChartViewModel { TitleText = "Types", YAxisTitleText = "Types", Id = "chartTypes" };
+
+        var ch4TotalMethane = new List<LineSeriesData>();
+        var fluorinatedGasesTotal = new List<LineSeriesData>();
+        var co2CarbonDioxideTotal = new List<LineSeriesData>();
+        var n2OTotalNitrousOxide = new List<LineSeriesData>();
+
+        model.Data = new List<Series>
+        {
+            new LineSeries { Name = "CH4 Total Methane", Data = ch4TotalMethane },
+            new LineSeries { Name = "Fluorinated Gases Total", Data = fluorinatedGasesTotal },
+            new LineSeries { Name = "O2 Carbon Dioxide Total", Data = co2CarbonDioxideTotal },
+            new LineSeries { Name = "N2O Nitrous Oxide Total", Data = n2OTotalNitrousOxide }
+        };
+        model.Labels = new List<string>();
+
+        foreach (var qtr in dataCache.EmissionType.OrderBy(y => y.Year))
+        {
+            ch4TotalMethane.Add(new LineSeriesData
+            {
+                Y = qtr.Ch4TotalMethane
+            });
+
+            fluorinatedGasesTotal.Add(new LineSeriesData
+            {
+                Y = qtr.FluorinatedGasesTotal
+            });
+
+            co2CarbonDioxideTotal.Add(new LineSeriesData
+            {
+                Y = qtr.Co2CarbonDioxideTotal
+            });
+
+            n2OTotalNitrousOxide.Add(new LineSeriesData
+            {
+                Y = qtr.N2OTotalNitrousOxide
+            });
+
+            model.Labels.Add($"{qtr.Year}");
+        }
+
+        return View(model);
+    }
+}
